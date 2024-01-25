37cc10f679b3df15d3952829c56c5d22118be408
NameUnknown1337 <git>
2024-01-25 22:36:21 +0200

    MERGE MA

--- MtgPrint/ConsoleApplication1/ConsoleApplication1.cpp
+++ MtgPrint/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,14 +1,79 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <sstream>
+#include <stdio.h>
+#include <string.h>
+#include <regex>
 using namespace std;
 
+
 string cutAndAdd(string inputString) {
-    string cutString = inputString.substr(2);
+  string cutString = inputString.substr(2);
     string newString = "1 " + cutString;
     return newString;
 }
+
+int getnumber(const string &liniuta) {
+    size_t space_index = liniuta.find(' ');
+    string linienoua;
+    int num;
+    if (space_index != string::npos) {
+        linienoua = liniuta.substr(0, space_index);
+
+        std::stringstream sstr(linienoua);
+        
+        if (sstr >> num && sstr.eof()) {
+            return num;
+        }
+        else {
+            throw std::invalid_argument("Invalid number format");
+        }
+    }
+    else {
+        throw std::invalid_argument("No number found");
+    }
+    num = stoi(linienoua);
+    return num;
+}
+
+void depack_multiple_rows(string filename) {
+    ifstream inputfile(filename);
+    ofstream outputinput("input_processed.txt");
+    if (!inputfile.is_open()) {
+        cout << "Error opening file " << filename << endl;
+        return;
+    }
+    
+    int startingnumber;
+    string line;
+    while (getline(inputfile, line)) {
+        
+        cout << line << "\n";
+        if (line.empty()||isspace(line[0])||line=="\n"|| line.empty() || line == "\r" || line == "\n" || line == "\r\n") {
+            continue;
+        }
+        startingnumber = getnumber(line);
+        cout << startingnumber<<'\n';
+        if (startingnumber == 1) {
+            outputinput << line << "\n";
+        }
+        else
+        {
+            regex expression(R"(\d+)");
+            string replacement = "1";
+
+            line = regex_replace(line, expression, replacement);
+
+           
+            for (int i = 0; i < startingnumber; i++) {
+                outputinput << line << "\n";
+            }
+        }
+    }
+
+}
+
 
 void processFile(string filename) {
     ifstream inputFile(filename);
@@ -20,145 +85,12 @@ void processFile(string filename) {
     }
 
     int rowNum = 0;
-    bool hasComment = false;
     string line;
-    string commentsAfterRow;
-    int repeatCount = 1;
+
 
     while (getline(inputFile, line)) {
-        line.erase(remove_if(line.begin(), line.end(), ::isspace), line.end());
-
-        // Check if the line is empty
-        if (line.length() == 0) {
-            continue;
-        }
-
-        // Check for "//" and store the text after it
-        /*if (line.find("//") != string::npos) {
-            hasComment = true;
-            commentsAfterRow = line.substr(line.find("//") + 2);
-        }*/
-
-        // Get the first number in the line
-        int startingNumber = stoi(line.substr(0, 2));
-
-        // Repeat the current line based on the starting number
-        switch (startingNumber) {
-            case 2:
-                repeatCount = 2;
-                break;
-        case 3:
-            repeatCount = 3;
-            break;
-            case 4:
-                repeatCount = 4;
-				break;
-                case 5:
-			repeatCount = 5;
-			break;
-            case 6:
-			repeatCount = 6;
-			break;
-            case 7:
-                repeatCount = 7;
-                break;
-                case 8:
-                    repeatCount = 8;
-                    break;
-                    case 9:
-                        repeatCount = 9;
-						break;
-
-        case 10:
-            repeatCount = 10;
-            break;
-        case 11:
-            repeatCount = 11;
-			break;
-		case 12:
-			repeatCount = 12;
-            break;
-            case 13:
-                repeatCount = 13;
-				break;
-				case 14:
-					repeatCount = 14;
-					break;
-
-        case 15:
-            repeatCount = 15;
-            break;
-        case 16:
-			repeatCount = 16;
-			break;
-        case 17:
-			repeatCount = 17;
-			break;
-            case 18:
-                repeatCount= 18;
-                break;
-            case 19:
-                				repeatCount = 19;
-				break;
-                case 20:
-                    repeatCount = 20;
-                    break;
-                case 21:    
-                    repeatCount = 21;
-					break;
-					case 22:
-						repeatCount = 22;
-						break;
-						case 23:
-							repeatCount = 23;
-							break;
-							case 24:
-								repeatCount = 24;
-								break;
-								case 25:
-									repeatCount = 25;
-									break;
-									case 26:
-										repeatCount = 26;
-										break;
-										case 27:
-											repeatCount = 27;
-											break;
-											case 28:
-												repeatCount = 28;
-												break;
-												case 29:
-													repeatCount = 29;
-													break;
-													case 30:
-														repeatCount = 30;
-														break;
-														case 31:
-															repeatCount = 31;
-															break;
-															case 32:
-																repeatCount = 32;
-																break;
-																case 33:
-																	repeatCount = 33;
-																	break;
-																	case 34:
-																		repeatCount = 34;
-																		break;
-																		case 35:
-																			repeatCount = 35;
-																			break;
-																	
-																	
-
-
-
-
-
-        default:
-            repeatCount = 1;
-            break;
-        }
+     
+        
 
         // Insert "BACK" rows if necessary
       /*  if (rowNum % 9 == 0 && hasComment) {
@@ -168,27 +100,20 @@ void processFile(string filename) {
             for (int i = 0; i < commentsAfterRow.length(); i++) {
                 outputFile << commentsAfterRow[i];
             }
-        }*/
+        }*/ 
+        outputFile << line << "\n";
+        rowNum++;
+
          if (rowNum % 9 == 0) {
             for (int i = 0; i < 9; i++) {
                 outputFile << "1 Back (EXTRA) 001\n";
+                rowNum = 0;
             }
         }
 
-        // Repeat the current line based on the repeat count
-        for (int i = 0; i < repeatCount; i++) {
-            cutAndAdd(line);
-            outputFile << line << endl;
-        }
-        repeatCount = 1;
 
-        // Write the current line
-        if (startingNumber < 2 || startingNumber > 36) {
 
-            outputFile << line << endl;
-        }
-
-        rowNum++;
+      
     }
 
     inputFile.close();
@@ -196,7 +121,77 @@ void processFile(string filename) {
 }
 
 int main() {
-    processFile("input.txt");
+    depack_multiple_rows("input.txt");
+   processFile("input_processed.txt");
 
     return 0;
 }
+/*#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    // Open input file
+    ifstream inputFile("input.txt");
+
+    // Read number of rows
+    int rows;
+    inputFile >> rows;
+    cout << rows;
+
+    // Initialize line counter and repeat counter
+    int lineCount = 0;
+    int repeatCount = 1;
+
+    // Read and process lines
+    while (lineCount < rows) {
+        // Read line
+        string line;
+        getline(inputFile, line);
+
+        // Extract number and check if it's greater than 1
+        int number;
+        istringstream iss(line);
+        iss >> number;
+        if (number > 1) {
+            // Repeat the line multiple times
+            for (int i = 0; i < number - 1; i++) {
+                cout << line << endl;
+            }
+
+            // Repeat the line once with number changed to 1
+            cout << "1 " << line.substr(2) << endl;
+
+            // Skip the specified number of lines
+            for (int i = 0; i < number; i++) {
+                getline(inputFile, line);
+                lineCount++;
+            }
+
+            // Reset repeat counter
+            repeatCount = 1;
+        }
+        else {
+            // Just print the line as is
+            cout << line << endl;
+        }
+
+        // Check for EXTRA section
+        if (lineCount % 9 == 0) {
+            // Print nine EXTRA lines
+            for (int i = 0; i < 9; i++) {
+                cout << "1 Back (EXTRA) 001\n";
+            }
+        }
+
+        // Increment line count
+        lineCount++;
+    }
+
+    // Close input file
+    inputFile.close();
+
+    return 0;
+}*/
